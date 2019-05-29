8b28b2bb7d224b4e2433a10981883e5a21e9033c
linasalve28 <lina.salve28@gmail.com>
2019-05-29 14:31:05 +0100

    Update tb.db.conn.php

--- dbn/tb.db.conn.php
+++ dbn/tb.db.conn.php
@@ -1,13 +1,13 @@
 <?php
 
 
-$db = new PDO('mysql:host=localhost;dbname=jdnxaorg_trustbase;charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$db = new PDO('mysql:host=localhost;dbname=jdnxaorg;charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
 
 function db_conn() {
   static $db = false;
   if (!$db) {
-$db = new PDO('mysql:host=localhost;dbname=jdnxaorg_trustbase;charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$db = new PDO('mysql:host=localhost;dbname=jdnxaorg;charset=utf8', 'root', '', array(PDO::ATTR_EMULATE_PREPARES => false,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
   }
   return $db;
 }
